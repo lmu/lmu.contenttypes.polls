0842166099173e7922a4f399a3c6f8222a1b9cbb
Alexander Loechel <Alexander.Loechel@lmu.de>
2015-03-03 00:05:50 +0100

    update based on collective.polls
    TODO: remove grok

--- setup.py
+++ setup.py
@@ -11,8 +11,20 @@ setup(name='lmu.contenttypes.polls',
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
-          "Framework :: Plone",
-          "Programming Language :: Python",
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Framework :: Plone',
+          'Framework :: Plone :: 4.2',
+          'Framework :: Plone :: 4.3',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+          'Operating System :: OS Independent',
+          'Programming Language :: JavaScript',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Office/Business :: News/Diary',
+          'Topic :: Software Development :: Libraries :: Python Modules',
       ],
       keywords='plone contenttype dexterity polls ratings',
       author='Alexander Loechel',
@@ -27,10 +39,37 @@ setup(name='lmu.contenttypes.polls',
       install_requires=[
           'setuptools',
           # -*- Extra requirements: -*-
+          'AccessControl',
+          'collective.z3cform.widgets >=1.0b3',
+          'plone.api',
+          'plone.app.dexterity [grok, relations]',
+          'plone.app.portlets',
+          'plone.app.referenceablebehavior',
+          'plone.directives.dexterity',
+          'plone.memoize',
+          'plone.portlets',
+          'plone.uuid',
+          'Products.CMFCore',
+          'Products.CMFPlone >=4.2',
+          'Products.GenericSetup',
+          'Products.statusmessages',
+          'setuptools',
+          'zope.component',
+          'zope.i18nmessageid',
+          'zope.interface',
+          'zope.schema',
       ],
+      extras_require={
+          'test': [
+              'plone.app.robotframework',
+              'plone.app.testing [robot] >=4.2.2',
+              'plone.dexterity',
+              'plone.testing',
+              'robotsuite',
+          ],
+      },
       entry_points="""
       # -*- Entry points: -*-
-
       [z3c.autoinclude.plugin]
       target = plone
       """,
--- src/lmu/contenttypes/polls/__init__.py
+++ src/lmu/contenttypes/polls/__init__.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
 
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
+from zope.i18nmessageid import MessageFactory
+
+# Set up the i18n message factory for our package
+MessageFactory = MessageFactory('collective.polls')
